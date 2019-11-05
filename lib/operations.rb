a1dd8f9945e72e58e2919c499894f43e4af27d14
Taylor Kraft <taylorkraft16@gmail.com>
2019-11-05 20:22:51 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,17 +1,17 @@
 def unsafe?(speed)
-if speed > 60
-  return true
+  if speed > 60
+    return true
   elsif speed < 40 
-  return true
-else
-  return false
-end 
+    return true
+  else
+    return false
+  end 
 end
 
 
 
 def not_safe?(speed)
-	
+	speed > 60 ? "true" : "false"
 end
 	
 
